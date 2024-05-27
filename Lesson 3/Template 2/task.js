8015c3f1b186bda680da00e48133c845b2bed3dc
“Glen <valerii.hrebennikov@hebron-academy.com>
2024-05-27 11:02:39 +0300

    HebronRocket

---
+++ Lesson 3/Template 2/task.js
@@ -0,0 +1,11 @@
+const main = document.getElementById("main_header");
+const ul = document.getElementById("ul");
+const li = document.getElementsByClassName("linkList");
+
+main.style.color = "green";
+ul.style.width = "400px"; 
+li.style.width = "50%";
+
+
+
+
