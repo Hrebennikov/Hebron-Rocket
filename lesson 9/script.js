2cd8ad10a19c7259271c92b2d5bcf67de88f9a37
Your GitHub Username <Your GitHub Email>
2024-06-28 00:08:35 +0300

    Task routine

--- lesson 9/script.js
+++ lesson 9/script.js
@@ -1,10 +1,10 @@
 // Task routine
 // Action #1
-function getUp(time) {
+function gotUp(time) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (time === 8) {
-                resolve("8:00")
+               return resolve("8:00")
             } else {
                 reject("I am too lazy to get up at 8:00")
             }
@@ -16,7 +16,7 @@ function getUp(time) {
 function selfCare() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("I'm done with morning self-care")
+           return resolve("I'm done with morning self-care")
         }, 500)
     });
 }
@@ -30,10 +30,22 @@ function breakfast() {
     });
 }
 
+// Action #4
+function goToWork(time) {
+    return new Promise ((resolve, reject) => {
+        setTimeout( () => {
+            if (time === "9:30") {
+                resolve("I made it to work, everything is fine");
+            } else {
+                reject("I overslept and was late for work");
+            }
+        }, 2000)
+    });
+}
 
 
-
-getUp(8)
+// Output
+gotUp(8)
 .then(gotUp => {
     console.log("I got up at", gotUp);
 
@@ -46,7 +58,33 @@ getUp(8)
 })
 .then(value => {
     console.log(value);
+
+    return goToWork("9:30")
+})
+.then(value => {
+    console.log(value);
 })
 .catch(reason => {
     console.error(reason);
 })
+
+// Or
+async function routine() {
+    try {
+    const getUp = await gotUp(8)
+    console.log("I got up at", getUp);
+
+    const care = await selfCare()
+    console.log(care);
+
+    const ate = await breakfast()
+    console.log(ate);
+
+    const cameOut = await goToWork("9:30")
+    console.log(cameOut);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+routine()
